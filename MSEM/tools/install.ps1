2fa45caca10aa9f8a5efcb19db86d741512e8fa7
Eradev <admin@eradev.com>
2025-07-30 01:37:35 -0400

    Add install script

---
+++ MSEM/tools/install.ps1
@@ -0,0 +1,11 @@
+$customSource = "..\custom"
+$forgeCustomDestination = "$($env:APPDATA)\Forge"
+
+Copy-Item -Path $customSource -Destination $forgeCustomDestination -Recurse -Force
+Write-Host "Folder copied from '$customSource' to '$forgeCustomDestination'."
+
+$picsSource = "..\pics"
+$forgePicsDestination = "$($env:LOCALAPPDATA)\Forge\Cache"
+
+Copy-Item -Path $picsSource -Destination $forgePicsDestination -Recurse -Force
+Write-Host "Folder copied from '$picsSource' to '$forgePicsDestination'."
