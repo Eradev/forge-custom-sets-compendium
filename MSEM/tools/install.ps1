ef48ab8c1539d28ef1bf0970997e7fb3c30d1f32
Eradev <admin@eradev.com>
2025-08-17 01:46:32 -0400

    [MSEM] Update tools

--- MSEM/tools/install.ps1
+++ MSEM/tools/install.ps1
@@ -2,17 +2,31 @@ param(
     [bool]$images = $false
 )
 
+$prefix = "MSEM"
 $customSource = "..\custom"
 $forgeCustomDestination = "$($env:APPDATA)\Forge"
+
+# Remove existing folders
+Get-ChildItem -Path $forgeCustomDestination -Recurse -Directory -Force | Where-Object { $_.Name -eq $prefix } | ForEach-Object {
+    Remove-Item -Path $_.FullName -Recurse -Force
+    Write-Host "Deleted folder: $($_.FullName)"
+}
+
+# Remove existing edition files
+Get-ChildItem -Path $forgeCustomDestination -Recurse -File -Force | Where-Object {
+    $_.Extension -eq ".txt" -and $_.Name -like "$prefix*.txt"
+} | ForEach-Object {
+    Remove-Item -Path $_.FullName -Force
+    Write-Host "Deleted file: $($_.FullName)"
+}
 
 Copy-Item -Path $customSource -Destination $forgeCustomDestination -Recurse -Force
 Write-Host "Folder copied from '$customSource' to '$forgeCustomDestination'."
 
-
 if ($images) {
-  $picsSource = "..\pics"
-  $forgePicsDestination = "$($env:LOCALAPPDATA)\Forge\Cache"
+    $picsSource = "..\pics"
+    $forgePicsDestination = "$($env:LOCALAPPDATA)\Forge\Cache"
 
-  Copy-Item -Path $picsSource -Destination $forgePicsDestination -Recurse -Force
-  Write-Host "Folder copied from '$picsSource' to '$forgePicsDestination'."
+    Copy-Item -Path $picsSource -Destination $forgePicsDestination -Recurse -Force
+    Write-Host "Folder copied from '$picsSource' to '$forgePicsDestination'."
 }
